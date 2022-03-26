b2e5da50e292e27a6a91a79a3b90500a3ec96ecf
JarateKing <scmacintosh@upei.ca>
2022-03-26 10:18:58 -0300

    added stub structs

--- src/fullogs.h
+++ src/fullogs.h
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <vector>
+
 namespace Fullogs {
+	// structs
+	struct LogItem {
+		int level;
+		string message;
+	};
+	
+	struct LogItemsResult {
+		std::vector<LogItem> items;
+		
+		void printToStdout() {
+			for (int i = 0; i < items.size(); i++) {
+				cout << items[i].message << '\n';
+			}
+		}
+	};
+	
+	vector<LogItem> allItems;
+	
 	// API for writing logs
 	static void log() {
 		
