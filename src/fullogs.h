b7039811d16e7df8ccc09f52c6b27071096ed2f8
JarateKing <scmacintosh@upei.ca>
2022-03-26 10:02:04 -0300

    added stub API for logging

---
+++ src/fullogs.h
@@ -0,0 +1,21 @@
+namespace Fullogs {
+	// API for writing logs
+	static void log() {
+		
+	}
+	static void debug() {
+		
+	}
+	static void alert() {
+		
+	}
+	static void warn() {
+		
+	}
+	static void error() {
+		
+	}
+	static void fatal() {
+		
+	}
+}
