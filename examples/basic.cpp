9c1a5525da1cdc7002ec70fcde95b18b87e12c12
JarateKing <scmacintosh@upei.ca>
2022-03-27 01:33:38 -0300

    testing formatting string

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -4,5 +4,5 @@ int main() {
 	Fullogs::log({"Starting Main", "Hello", "Initial Log"});
 	Fullogs::log({"Starting Main", "World!", "Second Log"});
 	
-	Fullogs::getAllLogs().printToStdout();
+	Fullogs::getAllLogs().printToStdout("{level} at {time}:\n{context}\n{message}\n{help}\n\n");
 }
