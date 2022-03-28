32257d4b4d3538bba2f2487e06716f4b60a38897
JarateKing <scmacintosh@upei.ca>
2022-03-28 08:16:55 -0300

    added start of tags setup

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,8 +1,8 @@
 #include "../src/fullogs.h"
 
 int main() {
-	fullogs_log("Starting Main", "Hello", "Initial Log");
-	fullogs_log("Starting Main", "World!", "Second Log");
+	fullogs_log("Starting Main", "Hello", "Initial Log", {});
+	fullogs_log("Starting Main", "World!", "Second Log", {});
 	
 	fullogs_getAllLogs().printToStdout("{level} at {time} in {file}@{line} ({function}):\n{context}\n{message}\n{help}\n\n");
 }
