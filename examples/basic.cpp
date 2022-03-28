12d2ca98ec6c80113107357c045bd0a9caa4eb0f
JarateKing <scmacintosh@upei.ca>
2022-03-28 08:24:40 -0300

    added general tag to example

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,8 +1,8 @@
 #include "../src/fullogs.h"
 
 int main() {
-	fullogs_log("Starting Main", "Hello", "Initial Log", {});
-	fullogs_log("Starting Main", "World!", "Second Log", {});
+	fullogs_log("Starting Main", "Hello", "Initial Log", {"general"});
+	fullogs_log("Starting Main", "World!", "Second Log", {"general"});
 	
-	fullogs_getAllLogs().printToStdout("{level} at {time} in {file}@{line} ({function}):\n{context}\n{message}\n{help}\n\n");
+	fullogs_getAllLogs().printToStdout("{level} at {time} in {file}@{line} ({function}):\n{context}\n{message}\n{help}\n{tags}\n\n");
 }
