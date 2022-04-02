6438ab354c8325461a22a49ebd4f16be82342a3d
JarateKing <scmacintosh@upei.ca>
2022-04-01 23:39:22 -0300

    added basic example basic

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,13 +1,12 @@
 #include "../src/fullogs.h"
 
 int main() {
-	fullogs_logger loggerOne("1 {message}\n");
-	fullogs_logger loggerTwo("2 {message}\n");
-	
-	fullogs_listen({loggerOne.getNewLogs().printToStdout();});
-	fullogs_listen({loggerTwo.getNewLogs().printToStdout();});
-	
+	// write logs within code
 	fullogs_log("Starting Main", "Hello", "Initial Log", {"general"});
 	fullogs_log("Starting Main", "World!", "Second Log", {"general"});
 	fullogs_log("Starting Main", "Yup!", "Third Log", {"general"});
+	
+	// handle logs with a logger
+	fullogs_logger logger("{message}\n");
+	logger.getNewLogs().printToStdout();
 }
