8bf4b502d62242d812f42e2f8427d33f968f58cd
JarateKing <scmacintosh@upei.ca>
2022-04-08 08:02:07 -0300

    added mdsource converter

---
+++ doc/gen_examples.py
@@ -0,0 +1,23 @@
+import subprocess
+
+toprint = []
+with open('examples.mdsource', 'r') as file:
+	for line in file.readlines():
+		words = line.split()
+		if (len(words) > 0 and words[0] == '@@include'):
+			with open(words[1], 'r') as subfile:
+				for subline in subfile.readlines():
+					toprint.append(subline)
+		elif (len(words) > 0 and words[0] == '@@run'):
+			cmd = subprocess.run(line[5:], shell=True)
+		elif (len(words) > 0 and words[0] == '@@output'):
+			cmd = subprocess.run(line[8:], shell=True, capture_output=True)
+			output = cmd.stdout.decode()
+			output = output.replace('\r\n', '\n')
+			output = output.replace('\n\n', '\n')
+			toprint.append(output)
+		else:
+			toprint.append(line)
+
+with open('examples.md', 'w') as file:
+	file.writelines(toprint)
